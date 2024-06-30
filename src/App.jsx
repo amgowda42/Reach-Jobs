0119700571ea56631a02f17c1099f8a93e9e5971
Annappa Gowda <annappg2020@gmail.com>
2024-06-30 19:13:36 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 const App = () => {
-  return <div>App</div>;
+
+  const name = ["Annappa", "samith","Rahl"];
+  const loggedIn = true
+  return (
+    <div>
+      App
+      <p>list of names</p>
+      <ul>
+        {name.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {loggedIn && <h1>Hello Member</h1>}
+    </div>
+  );
 };
 
 export default App;
