793854935c6cae74b0e33a24d3c1e4548e7a21f1
Annappa Gowda <annappag2020@gmail.com>
2025-01-21 12:53:30 +0530

    Updates in api.js

--- src/lib/features/api.jsx
+++ src/lib/features/api.jsx
@@ -1,1 +1,24 @@
-//api configurations
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const BASE_URL = "http://api.stickyhr.com:8080";
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+
+  prepareHeaders: (headers, { getState }) => {
+    const state = getState();
+    if (state.auth && state.auth.token) {
+      headers.set("Authorization", `Bearer ${state.auth.token}`);
+    } else {
+      const token = Cookies.get("accessToken");
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+    }
+    return headers;
+  },
+});
+const api = createApi({
+  reducerPath: "api",
+  baseQuery: baseQuery,
+  endpoints: () => ({}),
+});
+export default api;
